33ec1e00a7495da4bd7738f764f3526cd2a2834c
Hwang Ji Hun <hhhhicode@gmail.com>
2022-12-09 02:36:59 +0900

    외부 회원가입 요청 응답 추가

--- src/main/java/hwangjihun/members/controller/ExternalFormMemberController.java
+++ src/main/java/hwangjihun/members/controller/ExternalFormMemberController.java
@@ -1,9 +1,15 @@
 package hwangjihun.members.controller;
 
+import hwangjihun.members.domain.members.MemberConst;
 import hwangjihun.members.model.Member;
+import hwangjihun.members.model.dto.MemberAddDto;
 import hwangjihun.members.model.dto.MemberLoginDto;
 import hwangjihun.members.service.MemberService;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -19,7 +25,7 @@ public class ExternalFormMemberController {
     }
 
     @ResponseBody
-    @PostMapping("/login")
+    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     public Member login(@RequestBody MemberLoginDto memberLoginDto) {
         Optional<Member> loginMember = memberService.login(memberLoginDto);
         if (loginMember.isEmpty()) {
@@ -27,5 +33,25 @@ public class ExternalFormMemberController {
         }
 
         return loginMember.get();
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/add", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Member add(@Validated @RequestBody MemberAddDto memberAddDto, BindingResult bindingResult) {
+
+        //Global Error Validation
+        Optional<Member> findMember = memberService.findByUserId(memberAddDto.getUserId());
+        if (findMember.isPresent()) {
+            bindingResult.addError(new FieldError("memberAddDto", "userId",
+                    memberAddDto.getUserId(), false, null, null, "중복된 아이디가 있습니다."));
+        }
+        boolean isUserIdDuplicate = memberService.isUserIdDuplicate(memberAddDto.getUserId());
+        if (isUserIdDuplicate || bindingResult.hasErrors()) {
+            return new Member();
+        }
+
+        //Validation Pass Logic
+
+        return memberService.addMember(memberAddDto);
     }
 }
